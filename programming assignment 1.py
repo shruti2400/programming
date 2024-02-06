8fab2641197ee62ccbd609cd39f7ff5521ce1a8f
shruti2400 <128717070+shruti2400@users.noreply.github.com>
2024-02-06 21:11:51 +0530

    Add files via upload

---
+++ programming assignment 1.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#1. Write a Python program to print "Hello Python"?
+
+print("hello Python")
+
+
+# In[2]:
+
+
+#2. Write a Python program to do arithmetical operations addition and division.?
+
+a = 4
+b = 5 
+
+#addition
+print(a+b)
+#division
+print(a-b)
+
+
+
+# In[3]:
+
+
+# 3. Write a Python program to find the area of a triangle?
+
+height = 100
+base = 25
+
+area = height*base/2
+
+print("Area of triangle:", area)
+
+
+# In[4]:
+
+
+# 4. Write a Python program to swap two variables?
+
+a = 6
+b = 7
+
+a , b = b ,a
+
+print(a)
+print(b)
+
+
+# In[6]:
+
+
+# Write a Python program to generate a random number?
+
+import random
+
+randomnum = random.randint(1,100)
+print("random number : ", randomnum)
+
+
+# In[ ]:
+
+
+
+
